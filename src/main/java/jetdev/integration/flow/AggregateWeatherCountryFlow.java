ceaa044825065ea01f4bf6e2d6813c3c471f5a97
Gauthier Solignac <gauthier@solignac.ovh>
2025-11-27 17:54:43 +0100

    :sparkles: Aggregates weather and country data

--- src/main/java/jetdev/integration/flow/AggregateWeatherCountryFlow.java
+++ src/main/java/jetdev/integration/flow/AggregateWeatherCountryFlow.java
@@ -1,15 +1,38 @@
 package jetdev.integration.flow;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.store.SimpleMessageStore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
+@Slf4j
 public class AggregateWeatherCountryFlow {
+
     @Bean
     public IntegrationFlow aggregateWeatherCountryFlowProcess() {
-        return IntegrationFlow.from("aggregateWeatherCountry")
-                .log(m -> "Aggregating weather and country data: " + m.getPayload())
+        return IntegrationFlow
+                .from("aggregateWeatherCountry")
+                .aggregate(a -> a
+                        .correlationStrategy(m -> m.getHeaders().get("correlationId"))
+                        .releaseStrategy(g -> g.size() == 3)
+                        .messageStore(new SimpleMessageStore())
+                        .outputProcessor(group -> {
+                            Map<String, Object> aggregatedPayload = new HashMap<>();
+                            group.getMessages().forEach(message -> {
+                                @SuppressWarnings("unchecked")
+                                Map<String, Object> payload = (Map<String, Object>) message.getPayload();
+                                aggregatedPayload.putAll(payload);
+                            });
+                            log.info("Aggregating group with {}", group.size());
+                            return aggregatedPayload;
+                        })
+                )
+                .log(m -> "Aggregated data: " + m.getPayload())
                 .channel("aggregateUserAndCountry")
                 .get();
     }
