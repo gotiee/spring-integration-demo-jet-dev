eaff7b5c5410ad98aaafe4157fec4469e3b30b67
Gauthier Solignac <gauthier@solignac.ovh>
2025-11-27 17:56:05 +0100

    :sparkles: Aggregates user and country data.

--- src/main/java/jetdev/integration/flow/AggregateUserAndCountryFlow.java
+++ src/main/java/jetdev/integration/flow/AggregateUserAndCountryFlow.java
@@ -1,16 +1,37 @@
 package jetdev.integration.flow;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.store.SimpleMessageStore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
+@Slf4j
 public class AggregateUserAndCountryFlow {
     @Bean
     public IntegrationFlow aggregateUserAndCountryFlowProcess() {
         return IntegrationFlow
                 .from("aggregateUserAndCountry")
-                .log(m -> "Aggregating user and country data: " + m.getPayload())
+                .aggregate(a -> a
+                        .correlationStrategy(m -> m.getHeaders().get("correlationId"))
+                        .releaseStrategy(g -> g.size() == 2)
+                        .messageStore(new SimpleMessageStore())
+                        .outputProcessor(group -> {
+                            Map<String, Object> aggregatedPayload = new HashMap<>();
+                            group.getMessages().forEach(message -> {
+                                @SuppressWarnings("unchecked")
+                                Map<String, Object> payload = (Map<String, Object>) message.getPayload();
+                                aggregatedPayload.putAll(payload);
+                            });
+                            log.info("Aggregating group with {}", group.size());
+                            return aggregatedPayload;
+                        })
+                )
+                .log(m -> "Aggregated data: " + m.getPayload())
                 .channel("nullChannel")
                 .get();
     }
