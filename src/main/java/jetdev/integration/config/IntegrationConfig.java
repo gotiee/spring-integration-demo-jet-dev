3fb662427119c423444c18c19fa0f3d55e635188
Gauthier Solignac <gauthier@solignac.ovh>
2025-11-27 15:23:08 +0100

    :sparkles: Implements CSV file ingestion flow

---
+++ src/main/java/jetdev/integration/config/IntegrationConfig.java
@@ -0,0 +1,29 @@
+package jetdev.integration.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.config.EnableIntegration;
+
+import java.util.List;
+
+@Configuration
+@EnableIntegration
+public class IntegrationConfig {
+
+    private static final List<String> CHANNEL_NAMES = List.of(
+            "tmp"
+    );
+
+    @Bean
+    public List<DirectChannel> directChannels() {
+        return CHANNEL_NAMES.stream()
+                .map(this::createDirectChannel)
+                .toList();
+    }
+
+    private DirectChannel createDirectChannel(String channelName) {
+        return new DirectChannel();
+    }
+}
---
+++ src/main/java/jetdev/integration/flow/CsvFileIntegrationFlow.java
@@ -0,0 +1,29 @@
+package jetdev.integration.flow;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.Pollers;
+import org.springframework.integration.file.dsl.Files;
+
+import java.io.File;
+
+@Slf4j
+@Configuration
+public class CsvFileIntegrationFlow {
+
+    @Bean
+    public IntegrationFlow csvFileIngestionFlow() {
+        return IntegrationFlow
+                .from(
+                        Files.inboundAdapter(new File("input"))
+                                .patternFilter("*.csv"),
+                        c -> c.poller(Pollers.fixedDelay(1000).maxMessagesPerPoll(1))
+                )
+                .handle(m -> log.info("Processing file line: {}", m.getPayload()))
+                .get();
+
+    }
+
+}
