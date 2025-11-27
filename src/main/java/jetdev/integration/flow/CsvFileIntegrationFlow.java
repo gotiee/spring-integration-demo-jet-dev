5927f87c879273265671def8e55069c762dc26ba
Gauthier Solignac <gauthier@solignac.ovh>
2025-11-27 15:34:32 +0100

    :sparkles: Adds correlation ID to processed file lines

--- src/main/java/jetdev/integration/flow/CsvFileIntegrationFlow.java
+++ src/main/java/jetdev/integration/flow/CsvFileIntegrationFlow.java
@@ -8,6 +8,7 @@ import org.springframework.integration.dsl.Pollers;
 import org.springframework.integration.file.dsl.Files;
 
 import java.io.File;
+import java.util.UUID;
 
 @Slf4j
 @Configuration
@@ -27,7 +28,11 @@ public class CsvFileIntegrationFlow {
                     String[] fields = line.split(",");
                     return fields.length >= 3 && fields[2].contains("@");
                 })
-                .handle(m -> log.info("Processing file line: {}", m.getPayload()))
+                .enrichHeaders(h -> h.headerFunction("correlationId", m -> UUID.randomUUID().toString()))
+                .handle(m -> {
+                    log.info("Correlation ID: {}", m.getHeaders().get("correlationId"));
+                    log.info("Processing file line: {}", m.getPayload());
+                })
                 .get();
     }
 
