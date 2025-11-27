2cecfe995acd5d00b080bb1117ee8635d9a116a7
Gauthier Solignac <gauthier@solignac.ovh>
2025-11-27 16:33:08 +0100

    :sparkles: Adds Nominatim service integration

---
+++ src/main/java/jetdev/integration/model/Nominatim.java
@@ -0,0 +1,18 @@
+package jetdev.integration.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Nominatim {
+    @JsonProperty("lat")
+    private String lat;
+
+    @JsonProperty("lon")
+    private String lon;
+
+    @JsonProperty("name")
+    private String name;
+}
---
+++ src/main/java/jetdev/integration/service/NominatimService.java
@@ -0,0 +1,38 @@
+package jetdev.integration.service;
+
+import jetdev.integration.model.Nominatim;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class NominatimService {
+    private final RestTemplate restTemplate;
+
+    @Value("${demo.apis.nominatim}")
+    private String nominatimApiUrl;
+
+    public Nominatim fetchLonLatFromCityAndCountry(String city, String country) {
+        String url = UriComponentsBuilder.fromUriString(nominatimApiUrl)
+                .path("/search")
+                .queryParam("q", city + " " + country)
+                .queryParam("format", "json")
+                .toUriString();
+
+        ResponseEntity<@NonNull Nominatim> response = restTemplate.exchange(
+                url,
+                HttpMethod.GET,
+                null,
+                Nominatim.class
+        );
+        return response.getBody();
+    }
+}
